81c207ab5eb1748b52eb1eb5247bf482c1109189
Bandhan singh <parbhatbandhan@gmail.com>
2021-08-27 23:47:04 +0530

    sum of two numbers

--- main.py
+++ main.py
@@ -1,1 +1,1 @@
-print("Hello world!! I am Bandhan Singh...")
+print("Hello world, I am Bandhan singh and i am new to python")
---
+++ output.py
@@ -0,0 +1,7 @@
+# this program is used to add two numbers and output the result
+a = 2
+b = 4
+
+c = a+b
+
+print("Sum of {0} and {1} is = {2} ".format(a, b, c))
