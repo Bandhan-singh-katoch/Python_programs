4f11d15ebd79ef51da6c8778948ed5b344d0c028
Bandhan singh <parbhatbandhan@gmail.com>
2021-08-29 23:53:14 +0530

    program for largest number

---
+++ greatest-number.py
@@ -0,0 +1,14 @@
+# greatest number among three numbers
+
+n1 = float(input("Enter the first number :"))
+n2 = float(input("Enter the second number :"))
+n3 = float(input("Enter the third number :"))
+
+if (n1 > n2) and (n1 > n3):
+    largest = n1
+elif (n2 > n1) and (n2 > n3):
+    largest = n2
+else:
+    largest = n3
+
+print("The largest number is :", largest)
