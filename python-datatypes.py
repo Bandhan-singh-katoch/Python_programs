facb81c5924e82abd45162ed503893aadf1e06a9
Bandhan singh <parbhatbandhan@gmail.com>
2021-08-31 23:38:43 +0530

    python data-types: python numbers

---
+++ python-datatypes.py
@@ -0,0 +1,13 @@
+# python data-types
+# python numbers
+a = 6
+print(a, " is of type", type(a))
+
+a = 6.0
+print(a, " is of type", type(a))
+
+a = 6+2j
+print(a, " is of type", type(a))
+
+# isinstance() function is used to check if an object belongs to a particular class.
+print(a, "is complex number: ", isinstance(a, complex))
