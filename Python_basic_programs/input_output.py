4e47ce880808dc1c33e1bdf82ab2a428de5a3560
Bandhan singh <parbhatbandhan@gmail.com>
2021-09-12 20:32:16 +0530

    output part complete

--- Python_basic_programs/input_output.py
+++ Python_basic_programs/input_output.py
@@ -1,4 +1,5 @@
 # I am writing only that code that i don't know
+# Output
 
 print(1,2,3,4)                  # 1 2 3 4
 print(1,2,3,4,sep='*')          # 1*2*3*4
@@ -10,3 +11,9 @@ print("Hlo {} I am {}".format("Bandhan","Bikesh"))
 print("Hlo {0} I am {1}".format("Bandhan","Bikesh"))
 print("Hlo {1} I am {0}".format("Bandhan","Bikesh"))
 print("Good morning {name}, where is section {section}".format(section='A',name='Ayush'))
+
+# Input: Syntax is input([prompt]) where prompt is the string that we want to display on screen
+num = input('Enter the number = ')
+print(num)         # 14
+print(type(num))   # <class 'str'> : Here the number entered in the form of string so we use int(), float() to convert into number
+print(int(num)+int(num))
