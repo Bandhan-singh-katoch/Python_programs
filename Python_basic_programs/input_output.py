494e537b52ed4db44eefb0e0d333eeb87f10d9d6
Bandhan singh <parbhatbandhan@gmail.com>
2021-09-12 20:05:11 +0530

    output formatting

---
+++ Python_basic_programs/input_output.py
@@ -0,0 +1,12 @@
+# I am writing only that code that i don't know
+
+print(1,2,3,4)                  # 1 2 3 4
+print(1,2,3,4,sep='*')          # 1*2*3*4
+print(1,2,3,4,sep='*',end='$')  # 1*2*3*4$
+print("\n")
+
+# output formatting i.e. str.format() , this method is used to make the output more attractive.
+print("Hlo {} I am {}".format("Bandhan","Bikesh"))
+print("Hlo {0} I am {1}".format("Bandhan","Bikesh"))
+print("Hlo {1} I am {0}".format("Bandhan","Bikesh"))
+print("Good morning {name}, where is section {section}".format(section='A',name='Ayush'))
