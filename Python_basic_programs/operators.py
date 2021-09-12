bf1a2032a77ad6e62f5a6e5e8229062d242e277a
Bandhan singh <parbhatbandhan@gmail.com>
2021-09-12 22:59:16 +0530

    arithmetic, comparison and logical operator done

---
+++ Python_basic_programs/operators.py
@@ -0,0 +1,21 @@
+# ------------Operator ------------
+# i will write only that part which i don't know
+
+# ------Arithmetic operators------
+x = 5; y = 2
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)    # Divides completely with fraction
+print(x//y)   # Quotient
+print(x%y)    # Remainder
+print(x**y)   # power operator
+
+# Comparison operators : > , < , >= , <= , != , == are the comparison operators and they return true or false
+
+# Logical operators: and , or , not are the logical operators in python.
+a = 1; b = False
+print("a and b = ",a and b)
+print("a or b = ",a or b)
+print("not a =", not a)
+
