ab9575b43ee0b08f70a2d6fb92e29200ac194f4a
Bandhan singh <parbhatbandhan@gmail.com>
2021-09-16 11:28:17 +0530

    function with parameters created

--- Python_basic_programs/functions.py
+++ Python_basic_programs/functions.py
@@ -6,3 +6,13 @@ def fun():
 # calling a function
 fun()
 
+# -------------  function created with parameters ---------------------#
+
+def oddeven(num):
+    if num%2 == 0:
+        print("even")
+    else:
+        print("odd")
+
+oddeven(3)
+oddeven(8)
