dc39444f022e1e4e5bf3a22e3d5550b79b119ac6
Bandhan singh <parbhatbandhan@gmail.com>
2021-09-18 23:17:38 +0530

    for loop starting

---
+++ Python_basic_programs/for_loop.py
@@ -0,0 +1,5 @@
+# =================  for loop =====================
+# for loop is used for the sequential iteration i.e. used for iterating over the iterable like tuple, list, string
+# SYNTAX:  for var in iterable:
+               # statements
+
