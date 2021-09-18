9b2dfac1324c462606022c5bd7dfec3d1268fabe
Bandhan singh <parbhatbandhan@gmail.com>
2021-09-18 23:37:08 +0530

    for loop with list, tuple, string, set and dictionary done

--- Python_basic_programs/for_loop.py
+++ Python_basic_programs/for_loop.py
@@ -3,3 +3,32 @@
 # SYNTAX:  for var in iterable:
                # statements
 
+# iterating over a list
+print("List iteration")
+l = ['i','am','in','list']
+for i in l:
+    print(i)
+
+# iteration over a tuple
+print("Tuple iteration")
+t = ('I','am','in','tuple')
+for i in t:
+    print(i)
+
+# iteration over a string
+print("String iteration")
+s = "Bandhan"
+for i in s:
+    print(i)
+
+# iteration over a set
+print("Set iteration")
+num_set = {3,2,5,6,-2}
+for i in num_set:
+    print(i)
+
+# dictionary iteration
+print("Dictionary iteration")
+d = {1:"Bandhan",2:"Singh",4:"katoch"}
+for i in d:
+    print(d[i])
