abc681eb3cfedf427604115f7e9f18da23bff99e
Bandhan singh <parbhatbandhan@gmail.com>
2021-09-18 19:07:51 +0530

    shorthand if else statement done

--- Python_basic_programs/ifelse.py
+++ Python_basic_programs/ifelse.py
@@ -31,3 +31,8 @@ else:
 # ---------------- shorthand if statement ------------------
 # shorthand if statement is used whenever we have only one statement in the if block. The statement can be put in the same line
 if i<6: print("{} is less than 6".format(i))
+
+# ---------------- shorthand if else statement ------------------
+# This can be used to write if else statement in a single line, where there is only one statement in both the if and else block
+a = 5
+print("{} is greater than 4".format(a)) if a>4 else print("{} is less than 4".format(a))
