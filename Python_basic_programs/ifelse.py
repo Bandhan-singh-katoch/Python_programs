9bcb8d49e28a8d0153c5545f99d2e57db615e0d4
Bandhan singh <parbhatbandhan@gmail.com>
2021-09-18 18:24:16 +0530

    if else statements

---
+++ Python_basic_programs/ifelse.py
@@ -0,0 +1,15 @@
+# =============== Python if else statements ===================
+
+# ---------------- if statement -----------------------
+if(4%2 == 0):
+    print("I am in if statement block")
+
+print("I am not in if block")
+
+# ---------------- if else statement ------------------
+i= 5
+if i>4:
+    print("i am greater than 4")
+else:
+    print("i am lesser than 4")
+
