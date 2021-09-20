4f094fe4109c7bdcbeeac62022f4c78b9bfa5780
Bandhan singh <parbhatbandhan@gmail.com>
2021-09-20 19:25:51 +0530

    python while loop with list and single statement while loop done

--- Python_basic_programs/while_loop.py
+++ Python_basic_programs/while_loop.py
@@ -5,3 +5,14 @@ while count < 4:
     count = count+1
     print(count,end=" ")          # 1 2 3 4
 
+print()
+# -------------------------- python while loop with list -----------------
+l = [34,2,8,44]
+while l:
+    print(l.pop(), end=" ")          # 44 8 2 34
+
+print()
+
+# ------------------------- single statement while loop ----------------
+count = 0
+while count < 4: count+=1; print(count, end=" ")       # 1 2 3 4  similar to 1st program at the top
