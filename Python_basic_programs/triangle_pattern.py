ec8da1b1e4e3aa1d04adfb7767def6d759b5aeab
Bandhan singh <parbhatbandhan@gmail.com>
2021-09-20 20:49:51 +0530

    triangular pattern created

---
+++ Python_basic_programs/triangle_pattern.py
@@ -0,0 +1,13 @@
+# ====================== triangular pattern ==================
+
+a,b = 0,0
+while b<5:
+    if a<=b:
+        print("*",end="  ")
+        a+=1
+        continue
+
+    if a>b:
+        b+=1
+        a=0
+        print("")
