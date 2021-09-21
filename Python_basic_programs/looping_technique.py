c436210992fe3a3697b0b422d3b9116d23ff244d
Bandhan singh <parbhatbandhan@gmail.com>
2021-09-21 12:04:30 +0530

    sorted() and reversed() done

--- Python_basic_programs/looping_technique.py
+++ Python_basic_programs/looping_technique.py
@@ -18,4 +18,24 @@ d = {"a":"Bandhan", "b":"Singh", "c":"Katoch"}
 for i,j in d.items():
     print(i,j)
 
+# using sorted() : sorted() is used to print the container in sorted order. It doesn't sort the container
+# but just prints the container in sorted order for 1 instance.
+
+l = [2,1,7,4,8,3,1]
+for a in sorted(l):
+    print(a,end=" ")          # 1 1 2 3 4 7 8
+
+# fo removing the similar elements we can use set.
+print()
+for a in sorted(set(l)):
+    print(a,end=" ")           # 1 2 3 4 7 8
+
+# reversed() : used to print the container in the reversed order. But no effect on the container.
+print()
+for i in reversed(range(1,10)):
+    print(i,end=" ")             # 9 8 7 6 5 4 3 2 1
+
+
+
+
 
