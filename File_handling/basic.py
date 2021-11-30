8d0d81563c4cf501107912b25d3c7fc704426f5a
Bandhan singh <parbhatbandhan@gmail.com>
2021-11-30 08:45:33 +0530

    read mode

--- File_handling/basic.py
+++ File_handling/basic.py
@@ -3,3 +3,12 @@ for i in file:
     print(i)
 
 # Output: I am bandhan singh from jammu
+
+# Working of read() mode
+f = open("geek.txt","r")
+print(f.read())
+# Output: I am bandhan singh from jammu
+
+f = open("geek.txt","r")
+print(f.read(4))
+# Output: I am
