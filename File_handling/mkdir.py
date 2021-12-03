e182693aa327262c819a928fe87c3a78be158908
Bandhan singh <parbhatbandhan@gmail.com>
2021-12-03 20:42:51 +0530

    program to create directory with verifying

--- File_handling/mkdir.py
+++ File_handling/mkdir.py
@@ -3,3 +3,9 @@ import os
 # program to create the directory or folder
 os.mkdir("sample")
 print("Folder created")
+
+# creating directory after verifying if the directory already exist
+if not os.path.isdir("samples"):
+    os.mkdir("samples")
+else:
+    print("This directory already exists")
