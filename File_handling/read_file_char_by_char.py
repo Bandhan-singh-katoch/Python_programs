59c2c1a3cb0fff0d41d23143abf80f9f2603d18d
Bandhan singh <parbhatbandhan@gmail.com>
2021-12-03 11:32:27 +0530

    program to read file character by character

--- File_handling/read_file_char_by_char.py
+++ File_handling/read_file_char_by_char.py
@@ -1,4 +1,5 @@
 # program to read file character by character i.e. one character then nxt line...
+# https://www.geeksforgeeks.org/python-program-to-read-character-by-character-from-a-file/
 file = open("gfg.txt","r")
 while 1:
     char = file.read(1)
