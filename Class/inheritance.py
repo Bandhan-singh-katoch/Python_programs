8e5209f6e63cdf81d50b063cf4b8100a19243642
Bandhan singh <parbhatbandhan@gmail.com>
2021-12-05 21:02:44 +0530

    inheritance program created

--- Class/inheritance.py
+++ Class/inheritance.py
@@ -16,3 +16,23 @@ print(person.getName(),person.isEmployeed())
 
 employee = Employee("Geek2")
 print(employee.getName(),employee.isEmployeed())
+
+# object class : in python object is root of all classes
+class Child(object):
+    def __init__(self,name,idno):
+        self.name = name
+        self.idno = idno
+
+    def display(self):
+        print(self.name)
+        print(self.idno)
+
+class Student(Child):
+    def __init__(self,name,idno,school,std):
+        self.school = school
+        self.std = std
+
+        Child.__init__(self,name,idno)
+
+a = Student("Bandhan","123","GGI","3")
+a.display()
